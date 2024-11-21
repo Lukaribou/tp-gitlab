9fba2e87dffaa4e0f19ef9dc2b1226890bf51314
Lukaribou <lucas.perroud@etudiant.univ-lr.fr>
2024-11-21 12:41:13 +0100

    remove rtds_action

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "rtds_action"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,7 +29,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-
-rtds_action_github_repo = "lperroud/my-arithmetic-lperroud"
-rtds_action_path = "./_build"
-rtds_action_github_token = os.environ.get("GITHUB_TOKEN")
