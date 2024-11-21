d4a2c509cc244f951b4bc96a8f96dd2abab0eda6
Lukaribou <lucas.perroud@etudiant.univ-lr.fr>
2024-11-21 10:27:46 +0100

    sphinx

--- src/my_arithmetic_lperroud/functions.py
+++ src/my_arithmetic_lperroud/functions.py
@@ -1,5 +1,18 @@
 def pgcd(a, b):
-    """Calcul du PGCD de 2 entiers a et b."""
+    """Calcul du PGCD de 2 entiers a et b.
+    
+    Parameters
+    ----------
+    a : int
+        Premier entier.
+    b : int
+        Deuxième entier.
+
+    Returns
+    -------
+    int
+        Le PGCD de a et b.
+    """
     while b != 0:
         a, b = b, a % b
     return a
